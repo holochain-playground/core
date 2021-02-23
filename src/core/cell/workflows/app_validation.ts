b2123adb39ed9a764d2d92c0c83ac7a5eee7e611
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-23 16:33:51 +0100

    Start app validation

--- src/core/cell/workflows/app_validation.ts
+++ src/core/cell/workflows/app_validation.ts
@@ -12,6 +12,16 @@ import {
 } from '../dht/put';
 import { integrate_dht_ops_task } from './integrate_dht_ops';
 import { WorkflowReturn, WorkflowType, Workspace } from './workflows';
+import {
+  AppEntryType,
+  DHTOp,
+  DHTOpType,
+  Element,
+  NewEntryHeader,
+  Update,
+} from '@holochain-open-dev/core-types';
+import { SimulatedDna, SimulatedZome } from '../../../dnas/simulated-dna';
+import { SimulatedZomeFunctionContext } from '../../hdk';
 
 // From https://github.com/holochain/holochain/blob/develop/crates/holochain/src/core/workflow/app_validation_workflow.rs
 export const app_validation = async (
@@ -24,16 +34,20 @@ export const app_validation = async (
 
   // TODO: actually validate
   for (const dhtOpHash of Object.keys(pendingDhtOps)) {
-    deleteValidationLimboValue(dhtOpHash)(worskpace.state);
-
     const validationLimboValue = pendingDhtOps[dhtOpHash];
 
-    const value: IntegrationLimboValue = {
-      op: validationLimboValue.op,
-      validation_status: ValidationStatus.Valid,
-    };
+    try {
+      await validate(worskpace.dna, validationLimboValue.op);
 
-    putIntegrationLimboValue(dhtOpHash, value)(worskpace.state);
+      deleteValidationLimboValue(dhtOpHash)(worskpace.state);
+
+      const value: IntegrationLimboValue = {
+        op: validationLimboValue.op,
+        validation_status: ValidationStatus.Valid,
+      };
+
+      putIntegrationLimboValue(dhtOpHash, value)(worskpace.state);
+    } catch (e) {}
   }
 
   return {
@@ -50,4 +64,61 @@ export function app_validation_task(): AppValidationWorkflow {
     details: undefined,
     task: worskpace => app_validation(worskpace),
   };
+}
+
+export async function validate(
+  dna: SimulatedDna,
+  dhtOp: DHTOp,
+  context: SimulatedZomeFunctionContext
+): Promise<boolean> {
+  switch (dhtOp.type) {
+    case DHTOpType.StoreEntry:
+      const header: NewEntryHeader = dhtOp.header.header.content;
+
+      if (
+        (header.entry_type as {
+          App: AppEntryType;
+        }).App
+      ) {
+        const app_entry_type = (header.entry_type as {
+          App: AppEntryType;
+        }).App;
+
+        const zome = dna.zomes[app_entry_type.zome_id];
+        const entryId = zome.entry_defs[app_entry_type.id];
+
+        const prefix = (header as Update).original_header_address
+          ? 'update'
+          : 'create';
+
+        const validate_functions = [
+          `validate`,
+          `validate_${prefix}_entry`,
+          `validate_${prefix}_entry_${entryId.id}`,
+        ];
+
+        const element: Element = {
+          entry: dhtOp.entry,
+          signed_header: dhtOp.header,
+        };
+
+        return validate_functions.every(fn =>
+          callIfExisting(fn, zome, context, element)
+        );
+      }
+    
+  }
+}
+
+export async function callIfExisting(
+  fnName: string,
+  zome: SimulatedZome,
+  context: SimulatedZomeFunctionContext,
+  element: Element
+): Promise<boolean> {
+  if (Object.keys(zome.zome_functions).includes(fnName)) {
+    return zome.zome_functions[fnName].call(context)(element);
+  } else {
+    return true;
+  }
 }
